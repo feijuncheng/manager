72cb26085cfb28638a9e627a1971720f09f4f1d5
GP-FEIJUNCHENG\gp-feijuncheng <fjc57216@163.com>
2018-01-05 16:42:04 +0800

    update

---
+++ src/config/dev.env.js
@@ -0,0 +1,7 @@
+const devConfig = {
+  sysName: '管理后台',
+  baseURL: 'localhost:8080',
+  tokenKeyName: '_token'
+}
+
+export default devConfig
---
+++ src/config/pro.env.js
@@ -0,0 +1,7 @@
+const proConfig = {
+  sysName: '管理后台',
+  baseURL: 'localhost:8080',
+  tokenKeyName: '_token'
+}
+
+export default proConfig
